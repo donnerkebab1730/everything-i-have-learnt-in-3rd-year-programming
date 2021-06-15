94592fb3fae85171fb0510c9748a44a3eb830821
donnerkebab1730 <80350015+donnerkebab1730@users.noreply.github.com>
2021-06-15 13:26:06 +0100

    Create day 1.py

---
+++ day 1.py
@@ -0,0 +1,25 @@
+//it was  made in scratch so now its in python
+
+#part 1
+
+ans = input("what is your name ")
+a = "hello " 
+print (a + ans)
+
+
+#part 2
+
+/ = /
+* = *
++ = +
+- = -
+
+a = input("enter number 1")
+b = input("enter number 2")
+ans = input("choose thing")
+if ans ==
+
+a = 
+b = 
+c = 
+total = 
